17bc0c2e174d337fe0c59c467446690fe956a53a
MB <autofakt5@gmail.com>
2020-09-26 16:04:03 -0700

    Add files via upload
    Created an Employee Class that stores an Employee ID number and name. Then created an EmployeeMap class that allows you to add Employee objects and look them up by their ID numbers. The EmployeeMap class uses a Map object to map ID numbers to Employee objects.

---
+++ EmployeeMap.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class Employee{
+    private int id;
+    private String name;
+    
+    public Employee(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    
+    public int getID(){
+        return this.id;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
+
+class EmployeeMapClass{
+    private Map<Integer, Employee> workMap;
+    
+    public EmployeeMapClass(){
+        workMap = new HashMap<>();
+    }
+    
+    public void add(Employee e){
+        workMap.put(e.getID(), e);
+    }
+    
+    public Employee getEmployee(int id){
+        Employee temp = workMap.get(id);
+        return temp;
+    }
+    
+}
+
+public class EmployeeMap
+{
+	public static void main(String[] args) {
+	    EmployeeMapClass em = new EmployeeMapClass();
+        em.add(new Employee(1,"Eason"));
+        em.add(new Employee(2,"Lisa"));
+        em.add(new Employee(3,"Anna"));
+        em.add(new Employee(4,"Olivia"));
+        em.add(new Employee(5,"Madelyn"));
+        em.add(new Employee(6,"Sam"));
+        em.add(new Employee(7,"Will"));
+        em.add(new Employee(8,"Johnson"));
+  
+		Scanner keyboard = new Scanner(System.in);
+		int option;
+		boolean go = true;
+		while(go){
+		    System.out.print("Please enter the employee ID or Negative Number to Exit: ");
+		    option = keyboard.nextInt();
+		    if (option < 0){
+		        go = false;
+		        break;}
+		    Employee temp = em.getEmployee(option);
+		    if(temp == null )
+		        System.out.println("This employee id doesn't exist!\n");
+		    else
+		        System.out.println("This employee is: " +temp.getName());
+		    
+		}
+		System.out.println("Exiting the program!\n");
+	}
+}
